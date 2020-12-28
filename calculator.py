c071b8b752e43d262fa100fa8373bd1296e89ea6
Серафим <70561906+ItYaS@users.noreply.github.com>
2020-12-28 16:44:29 +0700

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import sys
 import math
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets
 from gui import Ui_MainWindow
 
 
@@ -15,23 +15,20 @@ class MainWin(QtWidgets.QMainWindow):
         self.operation = None
         self.text = ''
 
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QtGui.QIcon('image/image.jpg'))
-
         # если не удалять, то будет баг
         self.delete_all()
 
         # функции вывода чисел
-        self.ui.zero.clicked.connect(lambda: self.print_num('0'))
-        self.ui.one.clicked.connect(lambda: self.print_num('1'))
-        self.ui.two.clicked.connect(lambda: self.print_num('2'))
-        self.ui.three.clicked.connect(lambda: self.print_num('3'))
-        self.ui.four.clicked.connect(lambda: self.print_num('4'))
-        self.ui.five.clicked.connect(lambda: self.print_num('5'))
-        self.ui.six.clicked.connect(lambda: self.print_num('6'))
-        self.ui.seven.clicked.connect(lambda: self.print_num('7'))
-        self.ui.eight.clicked.connect(lambda: self.print_num('8'))
-        self.ui.nine.clicked.connect(lambda: self.print_num('9'))
+        self.ui.zero.clicked.connect(lambda: self.numbers('0'))
+        self.ui.one.clicked.connect(lambda: self.numbers('1'))
+        self.ui.two.clicked.connect(lambda: self.numbers('2'))
+        self.ui.three.clicked.connect(lambda: self.numbers('3'))
+        self.ui.four.clicked.connect(lambda: self.numbers('4'))
+        self.ui.five.clicked.connect(lambda: self.numbers('5'))
+        self.ui.six.clicked.connect(lambda: self.numbers('6'))
+        self.ui.seven.clicked.connect(lambda: self.numbers('7'))
+        self.ui.eight.clicked.connect(lambda: self.numbers('8'))
+        self.ui.nine.clicked.connect(lambda: self.numbers('9'))
 
         # функции операций
         self.ui.addition.clicked.connect(lambda: self.addiction())
@@ -43,88 +40,65 @@ class MainWin(QtWidgets.QMainWindow):
         self.ui.factorial.clicked.connect(lambda: self.factorial())
 
         # другие функции
-        self.ui.result.clicked.connect(lambda: self.print_result())
-        self.ui.delete_all.clicked.connect(lambda: self.delete_all())
-        self.ui.delete_last.clicked.connect(lambda: self.delete_last())
-        self.ui.negative.clicked.connect(lambda: self.negative())
+        self.ui.num_sign.clicked.connect(lambda: self.num_sign())
         self.ui.point.clicked.connect(lambda: self.point())
+        self.ui.delete_last.clicked.connect(lambda: self.delete_last())
+        self.ui.delete_all.clicked.connect(lambda: self.delete_all())
+        self.ui.result.clicked.connect(lambda: self.print_result())
 
-    def print_num(self, num):
+    def numbers(self, num):
         self.text_list.append(str(num))
         self.text += str(num)
-        self.ui.res_line.setText(self.text)
-
-    def delete_last(self):
-        self.text = ''
-        del self.text_list[-1]
-        for i in self.list_text:
-            self.text += i
-        self.ui.res_line.setText(self.text)
-
-    def delete_all(self):
-        del self.text_list[:]
-        self.sign_operation = ''
-        self.text = ''
-        self.ui.res_line.setText(self.text)
-
-    def negative(self):
-        self.text_list.append('-')
-        self.text += '-'
-        self.ui.res_line.setText(self.text)
-
-    def point(self):
-        self.text_list.append('.')
-        self.text += '.'
-        self.ui.res_line.setText(self.text)
-
-    def radical(self):
-        self.operation = self.rad
-        self.sign_operation = '√'
-        self.text_list.append('√')
-        self.text += '√'
-        self.ui.res_line.setText(self.text)
-
-    def power(self):
-        self.operation = self.pow
-        self.sign_operation = '^'
-        self.text_list.append('^')
-        self.text += '^'
-        self.ui.res_line.setText(self.text)
-
-    def factorial(self):
-        self.operation = self.fac
-        self.sign_operation = '!'
-        self.text_list.append('!')
-        self.text += '!'
-        self.ui.res_line.setText(self.text)
+        self.ui.result_line.setText(self.text)
 
     def addiction(self):
         self.operation = self.add
         self.sign_operation = '+'
         self.text_list.append('+')
         self.text += '+'
-        self.ui.res_line.setText(self.text)
+        self.ui.result_line.setText(self.text)
 
     def subtraction(self):
         self.operation = self.sub
         self.sign_operation = '-'
         self.text_list.append('-')
         self.text += '-'
-        self.ui.res_line.setText(self.text)
+        self.ui.result_line.setText(self.text)
 
     def multiply(self):
         self.operation = self.mul
         self.sign_operation = '*'
         self.text_list.append('*')
         self.text += '*'
-        self.ui.res_line.setText(self.text)
+        self.ui.result_line.setText(self.text)
 
     def division(self):
         self.operation = self.div
         self.sign_operation = '/'
         self.text_list.append('/')
         self.text += '/'
-        self.ui.res_line.setText(self.text)
+        self.ui.result_line.setText(self.text)
+
+    def radical(self):
+        self.operation = self.rad
+        self.sign_operation = '√'
+        self.text_list.append('√')
+        self.text += '√'
+        self.ui.result_line.setText(self.text)
+
+    def power(self):
+        self.operation = self.pow
+        self.sign_operation = '^'
+        self.text_list.append('^')
+        self.text += '^'
+        self.ui.result_line.setText(self.text)
+
+    def factorial(self):
+        self.operation = self.fac
+        self.sign_operation = '!'
+        self.text_list.append('!')
+        self.text += '!'
+        self.ui.result_line.setText(self.text)
 
     def rad(self, num1, num2):
         return str(math.sqrt(float(num2)))
@@ -147,6 +121,29 @@ class MainWin(QtWidgets.QMainWindow):
     def div(self, num1, num2):
         return str(float(num1) / float(num2))
 
+    def num_sign(self):
+        self.text_list.append('-')
+        self.text += '-'
+        self.ui.result_line.setText(self.text)
+
+    def point(self):
+        self.text_list.append('.')
+        self.text += '.'
+        self.ui.result_line.setText(self.text)
+
+    def delete_last(self):
+        self.text = ''
+        del self.text_list[-1]
+        for i in self.text_list:
+            self.text += i
+        self.ui.result_line.setText(self.text)
+
+    def delete_all(self):
+        del self.text_list[:]
+        self.sign_operation = ''
+        self.text = ''
+        self.ui.result_line.setText(self.text)
+
     def print_result(self):
         self.text = ''
         for i in self.text_list:
@@ -154,7 +151,7 @@ class MainWin(QtWidgets.QMainWindow):
         num1, num2 = self.text.rsplit(self.sign_operation)
         self.text = self.operation(num1, num2)
         self.text_list = [str(self.text)]  # для того, что бы выполнять операции с ответом
-        self.ui.res_line.setText(self.text)
+        self.ui.result_line.setText(self.text)
 
 
 if __name__ == '__main__':
--- gui.py
+++ gui.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'calculator.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.2
-#
-# WARNING! All changes made in this file will be lost!
-
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
@@ -14,98 +5,111 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(321, 491)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.res_line = QtWidgets.QLineEdit(self.centralwidget)
-        self.res_line.setGeometry(QtCore.QRect(5, 5, 310, 51))
-        self.res_line.setStyleSheet("font: 12pt \"Lucida Console\";")
-        self.res_line.setText("")
-        self.res_line.setObjectName("res_line")
-        self.two = QtWidgets.QPushButton(self.centralwidget)
-        self.two.setGeometry(QtCore.QRect(80, 190, 80, 71))
-        self.two.setObjectName("two")
-        self.one = QtWidgets.QPushButton(self.centralwidget)
-        self.one.setGeometry(QtCore.QRect(0, 190, 80, 70))
-        self.one.setObjectName("one")
-        self.three = QtWidgets.QPushButton(self.centralwidget)
-        self.three.setGeometry(QtCore.QRect(160, 190, 80, 71))
-        self.three.setObjectName("three")
-        self.four = QtWidgets.QPushButton(self.centralwidget)
-        self.four.setGeometry(QtCore.QRect(0, 260, 80, 70))
-        self.four.setObjectName("four")
-        self.five = QtWidgets.QPushButton(self.centralwidget)
-        self.five.setGeometry(QtCore.QRect(80, 260, 80, 70))
-        self.five.setObjectName("five")
-        self.six = QtWidgets.QPushButton(self.centralwidget)
-        self.six.setGeometry(QtCore.QRect(160, 260, 80, 70))
-        self.six.setObjectName("six")
-        self.seven = QtWidgets.QPushButton(self.centralwidget)
-        self.seven.setGeometry(QtCore.QRect(0, 330, 80, 70))
-        self.seven.setObjectName("seven")
-        self.eight = QtWidgets.QPushButton(self.centralwidget)
-        self.eight.setGeometry(QtCore.QRect(80, 330, 80, 70))
-        self.eight.setObjectName("eight")
-        self.nine = QtWidgets.QPushButton(self.centralwidget)
-        self.nine.setGeometry(QtCore.QRect(160, 330, 80, 70))
-        self.nine.setObjectName("nine")
-        self.zero = QtWidgets.QPushButton(self.centralwidget)
-        self.zero.setGeometry(QtCore.QRect(80, 400, 80, 70))
-        self.zero.setObjectName("zero")
-        self.negative = QtWidgets.QPushButton(self.centralwidget)
-        self.negative.setGeometry(QtCore.QRect(160, 400, 80, 70))
-        self.negative.setStyleSheet("font: 75 10pt \"MS Shell Dlg 2\";\n"
-"")
-        self.negative.setObjectName("negative")
-        self.point = QtWidgets.QPushButton(self.centralwidget)
-        self.point.setGeometry(QtCore.QRect(0, 400, 80, 70))
-        self.point.setStyleSheet("font: 75 10pt \"MS Shell Dlg 2\";")
-        self.point.setObjectName("point")
-        self.result = QtWidgets.QPushButton(self.centralwidget)
-        self.result.setGeometry(QtCore.QRect(240, 400, 80, 70))
-        self.result.setStyleSheet("font: 75 10pt \"MS Shell Dlg 2\";\n"
-"background-color: rgb(85, 170, 255);")
-        self.result.setObjectName("result")
-        self.division = QtWidgets.QPushButton(self.centralwidget)
-        self.division.setGeometry(QtCore.QRect(240, 330, 80, 70))
-        self.division.setStyleSheet("")
-        self.division.setObjectName("division")
-        self.multiply = QtWidgets.QPushButton(self.centralwidget)
-        self.multiply.setGeometry(QtCore.QRect(240, 260, 80, 70))
-        self.multiply.setStyleSheet("")
-        self.multiply.setObjectName("multiply")
-        self.subtraction = QtWidgets.QPushButton(self.centralwidget)
-        self.subtraction.setGeometry(QtCore.QRect(240, 190, 80, 70))
-        self.subtraction.setStyleSheet("")
-        self.subtraction.setObjectName("subtraction")
-        self.addition = QtWidgets.QPushButton(self.centralwidget)
-        self.addition.setGeometry(QtCore.QRect(240, 120, 80, 70))
-        self.addition.setStyleSheet("")
-        self.addition.setObjectName("addition")
-        self.delete_all = QtWidgets.QPushButton(self.centralwidget)
-        self.delete_all.setGeometry(QtCore.QRect(0, 60, 160, 61))
-        self.delete_all.setStyleSheet("")
-        self.delete_all.setObjectName("delete_all")
-        self.delete_last = QtWidgets.QPushButton(self.centralwidget)
-        self.delete_last.setGeometry(QtCore.QRect(160, 60, 160, 61))
-        self.delete_last.setStyleSheet("")
-        self.delete_last.setObjectName("delete_last")
-        self.radical = QtWidgets.QPushButton(self.centralwidget)
-        self.radical.setGeometry(QtCore.QRect(0, 120, 80, 70))
-        self.radical.setStyleSheet("")
-        self.radical.setObjectName("radical")
-        self.power = QtWidgets.QPushButton(self.centralwidget)
-        self.power.setGeometry(QtCore.QRect(80, 120, 80, 70))
-        self.power.setStyleSheet("")
-        self.power.setObjectName("power")
-        self.factorial = QtWidgets.QPushButton(self.centralwidget)
-        self.factorial.setGeometry(QtCore.QRect(160, 120, 80, 70))
-        self.factorial.setStyleSheet("")
-        self.factorial.setObjectName("factorial")
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 321, 21))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
+
+        self.result_line = QtWidgets.QLineEdit(self.centralwidget)
+        self.result_line.setGeometry(QtCore.QRect(5, 5, 310, 51))
+        self.result_line.setStyleSheet("font: 12pt \"Lucida Console\";")
+        self.result_line.setObjectName("result_line")
+
+        self.zero = QtWidgets.QPushButton(self.centralwidget)
+        self.zero.setGeometry(QtCore.QRect(80, 400, 80, 70))
+        self.zero.setObjectName("zero")
+
+        self.one = QtWidgets.QPushButton(self.centralwidget)
+        self.one.setGeometry(QtCore.QRect(0, 190, 80, 70))
+        self.one.setObjectName("one")
+
+        self.two = QtWidgets.QPushButton(self.centralwidget)
+        self.two.setGeometry(QtCore.QRect(80, 190, 80, 71))
+        self.two.setObjectName("two")
+
+        self.three = QtWidgets.QPushButton(self.centralwidget)
+        self.three.setGeometry(QtCore.QRect(160, 190, 80, 71))
+        self.three.setObjectName("three")
+
+        self.four = QtWidgets.QPushButton(self.centralwidget)
+        self.four.setGeometry(QtCore.QRect(0, 260, 80, 70))
+        self.four.setObjectName("four")
+
+        self.five = QtWidgets.QPushButton(self.centralwidget)
+        self.five.setGeometry(QtCore.QRect(80, 260, 80, 70))
+        self.five.setObjectName("five")
+
+        self.six = QtWidgets.QPushButton(self.centralwidget)
+        self.six.setGeometry(QtCore.QRect(160, 260, 80, 70))
+        self.six.setObjectName("six")
+
+        self.seven = QtWidgets.QPushButton(self.centralwidget)
+        self.seven.setGeometry(QtCore.QRect(0, 330, 80, 70))
+        self.seven.setObjectName("seven")
+
+        self.eight = QtWidgets.QPushButton(self.centralwidget)
+        self.eight.setGeometry(QtCore.QRect(80, 330, 80, 70))
+        self.eight.setObjectName("eight")
+
+        self.nine = QtWidgets.QPushButton(self.centralwidget)
+        self.nine.setGeometry(QtCore.QRect(160, 330, 80, 70))
+        self.nine.setObjectName("nine")
+
+        self.addition = QtWidgets.QPushButton(self.centralwidget)
+        self.addition.setGeometry(QtCore.QRect(240, 120, 80, 70))
+        self.addition.setObjectName("addition")
+
+        self.subtraction = QtWidgets.QPushButton(self.centralwidget)
+        self.subtraction.setGeometry(QtCore.QRect(240, 190, 80, 70))
+        self.subtraction.setObjectName("subtraction")
+
+        self.multiply = QtWidgets.QPushButton(self.centralwidget)
+        self.multiply.setGeometry(QtCore.QRect(240, 260, 80, 70))
+        self.multiply.setObjectName("multiply")
+
+        self.division = QtWidgets.QPushButton(self.centralwidget)
+        self.division.setGeometry(QtCore.QRect(240, 330, 80, 70))
+        self.division.setObjectName("division")
+
+        self.radical = QtWidgets.QPushButton(self.centralwidget)
+        self.radical.setGeometry(QtCore.QRect(0, 120, 80, 70))
+        self.radical.setObjectName("radical")
+
+        self.power = QtWidgets.QPushButton(self.centralwidget)
+        self.power.setGeometry(QtCore.QRect(80, 120, 80, 70))
+        self.power.setObjectName("power")
+
+        self.factorial = QtWidgets.QPushButton(self.centralwidget)
+        self.factorial.setGeometry(QtCore.QRect(160, 120, 80, 70))
+        self.factorial.setObjectName("factorial")
+
+        self.num_sign = QtWidgets.QPushButton(self.centralwidget)
+        self.num_sign.setGeometry(QtCore.QRect(160, 400, 80, 70))
+        self.num_sign.setStyleSheet("font: 75 10pt \"MS Shell Dlg 2\";\n")
+        self.num_sign.setObjectName("num_sign")
+
+        self.point = QtWidgets.QPushButton(self.centralwidget)
+        self.point.setGeometry(QtCore.QRect(0, 400, 80, 70))
+        self.point.setStyleSheet("font: 75 10pt \"MS Shell Dlg 2\";")
+        self.point.setObjectName("point")
+
+        self.delete_last = QtWidgets.QPushButton(self.centralwidget)
+        self.delete_last.setGeometry(QtCore.QRect(160, 60, 160, 61))
+        self.delete_last.setObjectName("delete_last")
+
+        self.delete_all = QtWidgets.QPushButton(self.centralwidget)
+        self.delete_all.setGeometry(QtCore.QRect(0, 60, 160, 61))
+        self.delete_all.setObjectName("delete_all")
+
+        self.result = QtWidgets.QPushButton(self.centralwidget)
+        self.result.setGeometry(QtCore.QRect(240, 400, 80, 70))
+        self.result.setStyleSheet("font: 75 10pt \"M" "S Shell Dlg 2\";\n" "background-color: rgb(85, 170, 255);")
+        self.result.setObjectName("result")
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -113,8 +117,11 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.two.setText(_translate("MainWindow", "2"))
+        MainWindow.setWindowTitle('Calculator')
+        MainWindow.setWindowIcon(QtGui.QIcon('image/logo.jpg'))
+
         self.one.setText(_translate("MainWindow", "1"))
+        self.two.setText(_translate("MainWindow", "2"))
         self.three.setText(_translate("MainWindow", "3"))
         self.four.setText(_translate("MainWindow", "4"))
         self.five.setText(_translate("MainWindow", "5"))
@@ -123,15 +130,17 @@ class Ui_MainWindow(object):
         self.eight.setText(_translate("MainWindow", "8"))
         self.nine.setText(_translate("MainWindow", "9"))
         self.zero.setText(_translate("MainWindow", "0"))
-        self.negative.setText(_translate("MainWindow", "+/-"))
-        self.point.setText(_translate("MainWindow", ","))
-        self.result.setText(_translate("MainWindow", "="))
-        self.division.setText(_translate("MainWindow", "/"))
-        self.multiply.setText(_translate("MainWindow", "*"))
-        self.subtraction.setText(_translate("MainWindow", "-"))
+
         self.addition.setText(_translate("MainWindow", "+"))
-        self.delete_all.setText(_translate("MainWindow", "C"))
-        self.delete_last.setText(_translate("MainWindow", "<="))
-        self.radical.setText(_translate("MainWindow", "√‎"))
+        self.subtraction.setText(_translate("MainWindow", "-"))
+        self.multiply.setText(_translate("MainWindow", "*"))
+        self.division.setText(_translate("MainWindow", "/"))
+        self.radical.setText(_translate("MainWindow", "√"))
         self.power.setText(_translate("MainWindow", "^"))
         self.factorial.setText(_translate("MainWindow", "n!"))
+
+        self.num_sign.setText(_translate("MainWindow", "+/-"))
+        self.point.setText(_translate("MainWindow", "."))
+        self.delete_last.setText(_translate("MainWindow", "<="))
+        self.delete_all.setText(_translate("MainWindow", "C"))
+        self.result.setText(_translate("MainWindow", "="))
